051697fc2cc50fb2eba4099e4eabeac0c7ee3827
Uchida-tech <spaller300@gmail.com>
2024-08-27 01:26:03 +0900

    tailwind css導入

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Notifications\ResetPasswordNotification; //追記
 
 class User extends Authenticatable
 {
@@ -43,5 +44,13 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    //パスワードリセットの設定
+    public function sendPasswordResetNotification($token): void
+    {
+        $url = route('password.reset', ['token' => $token]);  // 名前付きルートを利用
+
+        $this->notify(new ResetPasswordNotification($url));
     }
 }
